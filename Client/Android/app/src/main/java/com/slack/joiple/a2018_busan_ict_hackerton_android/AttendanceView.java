efa94bdaf7f2a72554539b176ba264efdc6efee7
Jaeguins <jsu4838@naver.com>
2018-07-19 23:50:33 +0900

    all activity now needs add

---
+++ Client/Android/app/src/main/java/com/slack/joiple/a2018_busan_ict_hackerton_android/AttendanceView.java
@@ -0,0 +1,12 @@
+package com.slack.joiple.a2018_busan_ict_hackerton_android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AttendanceView extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle saveInstanceBundle){
+        super.onCreate(saveInstanceBundle);
+        this.setContentView(R.layout.activity_attendence_view);
+    }
+}
