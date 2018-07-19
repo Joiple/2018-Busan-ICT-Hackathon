44a333ed5ff7c18aa0e5bea840c0f6a7e7d34da1
Jaeguins <jsu4838@naver.com>
2018-07-19 19:59:32 +0900

    NFC service generated : AID required(TODO)

---
+++ Client/Android/app/src/main/java/com/slack/joiple/a2018_busan_ict_hackerton_android/NfcService.java
@@ -0,0 +1,57 @@
+package com.slack.joiple.a2018_busan_ict_hackerton_android;
+
+import android.content.Intent;
+import android.nfc.cardemulation.HostApduService;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
+
+public class NfcService extends HostApduService {
+    private  final String SAMPLE_LOYALTY_CARD_AID = new NfcService().getString(R.string.AID);
+    private  final String SELECT_APDU_HEADER = "00A40400";
+    private  final byte[] SELECT_APDU = BuildSelectApdu(SAMPLE_LOYALTY_CARD_AID);
+    private  final String NFC = "NFC";
+    private  final byte[] SELECT_OK_SW = HexStringToByteArray("9000");
+    private  final byte[] UNKNOWN_CMD_SW = HexStringToByteArray("0000");
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Bundle extras = intent.getExtras();
+        return START_STICKY;
+    }
+
+    @Override
+    public byte[] processCommandApdu(byte[] bytes, Bundle bundle) {
+        if (Arrays.equals(SELECT_APDU, bytes)) {
+            Log.i(NFC, "nfc touched");
+            return SELECT_OK_SW;
+        } else {
+            return UNKNOWN_CMD_SW;
+        }
+    }
+
+    public byte[] BuildSelectApdu(String aid) {
+        // Format: [CLASS | INSTRUCTION | PARAMETER 1 | PARAMETER 2 | LENGTH | DATA]
+        return HexStringToByteArray(SELECT_APDU_HEADER + String.format("%02X", aid.length() / 2) + aid);
+    }
+
+    public static byte[] HexStringToByteArray(String s) throws IllegalArgumentException {
+        int len = s.length();
+        if (len % 2 == 1) {
+            throw new IllegalArgumentException("Hex string must have even number of characters");
+        }
+        byte[] data = new byte[len / 2];
+        // Allocate 1 byte per 2 hex characters
+        for (int i = 0; i < len; i += 2) {
+            // Convert each character into a integer (base-16), then bit-shift into place
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+    @Override
+    public void onDeactivated(int i) {
+        Log.i(NFC,"Deactiviated"+i);
+    }
+}
--- Client/Android/app/src/main/java/com/slack/joiple/a2018_busan_ict_hackerton_android/NfcTagging.java
+++ Client/Android/app/src/main/java/com/slack/joiple/a2018_busan_ict_hackerton_android/NfcTagging.java
@@ -1,12 +1,21 @@
 package com.slack.joiple.a2018_busan_ict_hackerton_android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class NfcTagging extends AppCompatActivity {
+    private Intent cardService;
     @Override
-    public void onCreate(Bundle saveInstanceBundle){
+    protected void onCreate(Bundle saveInstanceBundle){
         super.onCreate(saveInstanceBundle);
         this.setContentView(R.layout.activity_nfc_tagging);
+        cardService=new Intent(this,NfcService.class);
+        startService(cardService);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(cardService);
     }
 }
