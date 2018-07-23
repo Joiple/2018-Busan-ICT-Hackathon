ccee4a50acab3bc2966b918b0ca5c030e7e07a2c
SEUNGWOO JUNG <jsw9808@gmail.com>
2018-07-23 12:24:13 +0900

    login and password processing added

--- Server/mysql.py
+++ Server/mysql.py
@@ -19,13 +19,15 @@ class sql:
             except pymysql.err.OperationalError:
                 print("user이름과 password를 확인하세요")
 
-    def sign_in(self, info, dbflag):
-        sql = "INSERT INTO user (name, phone_number, birthday, email) VALUES (%s, %s, %s, %s)" if dbflag == 0 else "INSERT INTO company (name, location, address, crn, introduction, inoutcode) VALUES (%s, %s, %s, %s, %s, %s)"
+    def sign_in(self, info, dbflag, salt):
+        print('run')
+        sql = "INSERT INTO user (id, password, name, phone_number, birthday, email, salt) VALUES (%s, %s, %s, %s, %s, %s, %s)" if dbflag == 0 else "INSERT INTO company (name, location, address, crn, introduction, inoutcode) VALUES (%s, %s, %s, %s, %s, %s)"
         with self.connection.cursor() as cursor:
             #sql = "INSERT INTO {type} (name, phone_number, birthday, email) VALUES (%s, %s, %s, %s)"
             if dbflag == 0:
+                print(info)
                 cursor.execute(
-                    sql, (info['name'], info['phone_number'], info['birthday'], info['email']))
+                    sql, (info['id'], info['password'], info['name'], info['phone_number'], info['birthday'], info['email'], salt)) # 원래는 암호화 키값을 다른 서버에 보관하여야 한다.
             else:
                 cursor.execute(sql, (info['name'], info['location'],
                                      info['address'], info['crn'], info['introduction'],info['inoutcode']))
@@ -51,7 +53,14 @@ class sql:
         self.connection.commit()
         return 1
 
-    def get_info(self, flag):
+    def get_info(self, flag, **kwargs):
+        if flag == 10:
+            sql = "SELECT salt FROM user WHERE id='{}'".format(kwargs['id'])
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                return result[0]['salt']
+
         sql = "SELECT * FROM {}".format(self.infolist[int(flag)])
         with self.connection.cursor() as cursor:
             cursor.execute(sql)
@@ -59,6 +68,13 @@ class sql:
             print(result)
             return result
 
+    def login(self, info):
+        sql = "SELECT password FROM user WHERE id = '{}'".format(info['id'])
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql)
+            password = cursor.fetchall()
+            result = 1 if password == info['password'] else 0
+            return result
 
     def dblogout(self):
         self.connection.close()
