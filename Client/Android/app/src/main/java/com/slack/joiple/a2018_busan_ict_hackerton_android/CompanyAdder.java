bae9435c87a7a76f6d63e7f9a2f1cee8f6ff60cd
Jaeguins <jsu4838@naver.com>
2018-07-25 18:54:29 +0900

    company adder generated

--- Client/Android/app/src/main/java/com/slack/joiple/a2018_busan_ict_hackerton_android/CompanyAdder.java
+++ Client/Android/app/src/main/java/com/slack/joiple/a2018_busan_ict_hackerton_android/CompanyAdder.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 
 public class CompanyAdder extends AppCompatActivity {
     NetworkManager manager;
-    EditText name,address,number,description,phone,location;
+    EditText name,address,number,description,location;
     Button searchBtn;
     TextView status;
     String networkReturn="";
@@ -19,23 +19,39 @@ public class CompanyAdder extends AppCompatActivity {
         super.onCreate(saveInstanceBundle);
         this.setContentView(R.layout.activity_add_company);
         //TODO connect with layout
-        manager=new NetworkManager(getString(R.string.serverURL));
+        name=findViewById(R.id.nameEdit);
+        address=findViewById(R.id.addressEdit);
+        number=findViewById(R.id.crnEdit);
+        description=findViewById(R.id.introductionEdit);
+        location=findViewById(R.id.locationEdit);
+
         searchBtn=findViewById(R.id.searchBtn);
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(true){//TODO checking company adding
-                    //TODO adding Company to server;
-                    SharedPreferences pref=getSharedPreferences("user",MODE_PRIVATE);
-                    SharedPreferences.Editor editor=pref.edit();
-                    editor.putString("compName",name.getText().toString());
-                    editor.putString("compAddress",address.getText().toString());
+                String compName=name.getText().toString(),
+                        compAddress=address.getText().toString(),
+                        compCrn=number.getText().toString(),
+                        compIntroduction=description.getText().toString(),
+                        compLocation=location.getText().toString();
+                manager=new NetworkManager(getString(R.string.serverURL),"makecom");
+                manager.in.addItem("name",compName);
+                manager.in.addItem("location",compLocation);
+                manager.in.addItem("address",compAddress);
+                manager.in.addItem("crn",compCrn);
+                manager.in.addItem("introduction",compIntroduction);
+                if(manager.execute()){//TODO checking company adding
+                        SharedPreferences pref = getSharedPreferences("user", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = pref.edit();
+                        editor.putString("compName", compName);
+                        editor.putString("compAddress", compAddress);
                 }else{
                     //TODO get errCode from server
                     status.setText("");
                 }
             }
         });
+
     }
 }
