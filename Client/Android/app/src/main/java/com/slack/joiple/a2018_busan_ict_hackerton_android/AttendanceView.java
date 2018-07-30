df81446462cb46d5cb3eee66fa7f40c680a4c882
wnelwldh <wnelwldh@naver.com>
2018-07-30 21:39:18 +0900

    글자 크기 폰트 색 로고넣기 버튼넣기

--- Client/Android/app/src/main/java/com/slack/joiple/a2018_busan_ict_hackerton_android/AttendanceView.java
+++ Client/Android/app/src/main/java/com/slack/joiple/a2018_busan_ict_hackerton_android/AttendanceView.java
@@ -35,6 +35,7 @@ public class AttendanceView extends AppCompatActivity {
             tmpLayout.addView(times);
             tmpLayout.addView(events);
             resultLayout.addView(tmpLayout);
+
         }
     }
 }
