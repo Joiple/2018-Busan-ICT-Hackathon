483c4448997c0f43a7a24bb196de3cdb533d1e94
wnelwldh <wnelwldh@naver.com>
2018-07-31 09:43:22 +0900

    ㅇㅇㅇㅇ

---
+++ Client/Android/app/src/main/java/com/slack/joiple/a2018_busan_ict_hackerton_android/Career.java
@@ -0,0 +1,23 @@
+package com.slack.joiple.a2018_busan_ict_hackerton_android;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+public class Career extends AppCompatActivity {
+
+
+    @Override
+
+    public void onCreate(Bundle saveInstanceBundle) {
+        super.onCreate(saveInstanceBundle);
+        this.setContentView(R.layout.activity_career);
+    }
+}
